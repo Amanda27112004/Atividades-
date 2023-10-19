722e80a83716b726dc37cfe38b35cfdf91b24a91
Amanda27112004 <147105353+Amanda27112004@users.noreply.github.com>
2023-10-19 19:01:26 -0300

    Script.js

---
+++ Script.js
@@ -0,0 +1,85 @@
+function menuDisplay() {
+
+  const lateral = document.getElementById('menu_lateral');
+
+  if (lateral.style.visibility == '') {
+
+    lateral.style.visibility = 'visible';
+
+  } else if (lateral.style.visibility == 'hidden') {
+
+    lateral.style.visibility = 'visible';
+
+  } else {
+
+    lateral.style.visibility = 'hidden';
+
+  }
+
+}
+
+
+
+function menuDisplay2() {
+
+  const lateral = document.getElementById('menu_lateral');
+
+  if (lateral.style.visibility == 'visible') {
+
+    lateral.style.visibility = 'hidden';
+
+  }
+
+}
+
+
+
+function loadConteudo(elemento) {
+
+  var divConteudo = document.getElementById('Conteudo');
+
+  divConteudo.setAttribute('class', 'divConteudo')
+
+  divConteudo.innerHTML = '';
+
+
+
+  var txtTitulo = document.createElement('h1');
+
+  txtTitulo.setAttribute('class', 'titulo');
+
+  txtTitulo.innerText = elemento.titulo;
+
+  divConteudo.appendChild(txtTitulo);
+
+
+
+  var txtResumo = document.createElement('p')
+
+  txtResumo.innerText = elemento.resumo;
+
+  divConteudo.appendChild(txtResumo);
+
+
+
+  if (elemento.links != undefined){
+
+    var links = document.createElement('a');
+
+    for (var i = 0; i < elemento.links.length; i++){
+
+      links.innerText = elemento.links[i].n;
+
+      links.setAttribute('class', 'links')
+
+      links.setAttribute('href', elemento.links[i].l);
+
+      divConteudo.appendChild(links);
+
+    }
+
+  }
+
+  
+
+}
